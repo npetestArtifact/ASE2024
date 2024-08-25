40c0ae54f0f9547c300c6fff670cf1b19ed99aaf
npetestArtifact <146438499+npetestArtifact@users.noreply.github.com>
2024-08-26 04:33:02 +0900

    Create get_main_results.sh

---
+++ scripts/get_main_results.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# GENERATE csv files for each tool and benchmark (# of found known NPEs)
+current=$(pwd)
+
+${current}/scripts/check_log_file.sh ./result/evosuite/evosuite_opt_result ./result/evosuite_result.csv
+${current}/scripts/check_log_file.sh ./result/npetest/npetest_result ./result/npetest_result.csv
+${current}/scripts/check_log_file.sh ./result/randoop/randoop_result ./result/randoop_result.csv
+
+${current}/scripts/merge_csv_files.sh ${current}/result/main_merge.csv
+
+python3 ${current}/scripts/build_table.py ${current}/result/main_merge.csv
