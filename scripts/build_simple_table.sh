f8dbb73a7574ec38fcf30f49d8904fb0a6d165dc
npetestArtifact <146438499+npetestArtifact@users.noreply.github.com>
2024-08-26 05:02:02 +0900

    Update build_simple_table.sh

--- scripts/build_simple_table.sh
+++ scripts/build_simple_table.sh
@@ -3,6 +3,9 @@
 # GENERATE csv files for each tool and benchmark (# of found known NPEs)
 current=$(pwd)
 
+cp ${current}/benchmarks/simple_benchmark.txt ${current}/benchmarks/benchmark_list.txt
+rm ${current}/result/*.csv
+
 ${current}/scripts/check_log_file.sh ./result/evosuite/evosuite_simple ./result/evosuite_result.csv
 ${current}/scripts/check_log_file.sh ./result/npetest/npetest_simple ./result/npetest_result.csv
 ${current}/scripts/check_log_file.sh ./result/randoop/randoop_simple ./result/randoop_result.csv
