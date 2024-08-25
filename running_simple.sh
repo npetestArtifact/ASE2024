ba539af48984dab97c962131b344a6b21a02c37c
npetestartifact <npetestartifact@gmail.com>
2024-08-25 07:52:04 +0000

    update

--- running_simple.sh
+++ running_simple.sh
@@ -11,9 +11,15 @@ tar -xvf subject_gits.tar.gz
 
 ./generate_dockerfile.sh
 
-python3 ./build_benchmark_parallel.py -j 2 --testing-type 1
+python3 ./build_benchmark_parallel.py -j ${1} --testing-type 1
 
 # BUILD DOCKER IMAGES FOR TESTING
 cd ../experiment
 ./generate_dockerfile.sh
-python3 ./build_docker_parallel.py -j 2
+python3 ./build_docker_parallel.py -j ${1}
+
+# RUNNING EXPERIMENTS
+cd ./scripts
+bash -c 'yes | NULL=0.1 JOB=${1} TIME_BUDGET=300 NPE_CLASS_ONLY=1 PYTHON=python3 TOOL=evosuite REPEAT=1 OUTPUT_DIR=/home/npetest/ase2024/result/evosuite/evosuite_simple ./run_experiment.sh'
+bash -c 'yes | NULL=0.1 JOB=${1} TIME_BUDGET=300 NPE_CLASS_ONLY=1 PYTHON=python3 TOOL=npetest REPEAT=1 OUTPUT_DIR=/home/npetest/ase2024/result/npetest/npetest_simple ./run_experiment.sh'
+bash -c 'yes | NULL=0.1 JOB=${1} TIME_BUDGET=300 NPE_CLASS_ONLY=1 PYTHON=python3 TOOL=randoop REPEAT=1 OUTPUT_DIR=/home/npetest/ase2024/result/randoop/randoop_simple ./run_experiment.sh'
